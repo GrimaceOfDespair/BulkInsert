633231726d0c4e1f3d45c7cf267f3eaf1c72a181
Grimace of Despair <GrimaceOfDespair@hotmail.com>
2013-11-18 02:15:00 +0100

    Truncate data when importing

--- Grimace.BulkInsert/Importer.cs
+++ Grimace.BulkInsert/Importer.cs
@@ -4,20 +4,26 @@ using System.Data.Common;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using Grimace.BulkInsert.Exceptions;
+using Grimace.BulkInsert.Extensions;
 using Grimace.BulkInsert.FormatFile;
 using Grimace.BulkInsert.Resources;
 using Grimace.BulkInsert.Schema;
+using log4net;
 
 namespace Grimace.BulkInsert
 {
   public class Importer : IDisposable
   {
+    public static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
     public DbConnection DbConnection { get; private set; }
     public string TableName { get; private set; }
     public string[] ColumnNames { get; private set; }
     public string FormatFile { get; private set; }
+    protected DbColumn[] DbColumns { get; private set; }
 
     public Importer(DbConnection sqlConnection, string tableName, params string[] columnNames)
     {
@@ -25,19 +31,21 @@ namespace Grimace.BulkInsert
       TableName = tableName;
       ColumnNames = columnNames;
 
-      var schemaProvider = new SchemaProvider(sqlConnection);
-      var dbColumns = schemaProvider.GetColumns(tableName);
+      DbColumns = new SchemaProvider(sqlConnection, columnNames)
+        .GetColumns(tableName).ToArray();
 
-      var formatFileBuilder = new FormatFileBuilder(schemaProvider);
-      var formatXml = formatFileBuilder.GetFormatXml(dbColumns);
+      FormatFile = FormatFileBuilder.CreateFormatFile(DbColumns);
 
-      var formatFile = Path.GetTempFileName();
-      File.WriteAllText(formatFile, formatXml);
+      if (Log.IsDebugEnabled)
+      {
+        Log.Debug(string.Format("Format file created: {0}", FormatFile.CreateLink()));
+      }
     }
 
     public void Import(IEnumerable<string[]> dataValues)
     {
       var pipeName = Guid.NewGuid().ToString();
+      var trimmedValues = dataValues.Select(Truncate);
 
       // Create a dummy pipe, because SqlServer will close the
       // first one before actually inserting any data, subsequently
@@ -45,10 +53,26 @@ namespace Grimace.BulkInsert
       // See also: http://stackoverflow.com/questions/2197017/can-sql-server-bulk-insert-read-from-a-named-pipe-fifo
       using (new NamedPipe(pipeName))
 
-      using (var namedPipe = new NamedPipe(pipeName, dataValues))
+      using (var namedPipe = new NamedPipe(pipeName, trimmedValues))
       {
         ExecuteInsert(namedPipe.Path);
       }
+    }
+
+    private string[] Truncate(string[] dataValueRow)
+    {
+      if (DbColumns.Length != dataValueRow.Length)
+      {
+        throw new ColumnMismatchException(string.Format("{0} fields found, while importer expected {1}", dataValueRow.Length, DbColumns.Length));
+      }
+
+      for (var column = 0; column < DbColumns.Length; column++)
+      {
+        var maxLength = DbColumns[column].MaxLength;
+        dataValueRow[column] = dataValueRow[column].Truncate(maxLength);
+      }
+
+      return dataValueRow;
     }
 
     private void ExecuteInsert(string namedPipePath)
