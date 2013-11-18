591593cc13f0e60f081067a77de0a43d4aacbef9
Grimace of Despair <GrimaceOfDespair@hotmail.com>
2013-11-18 02:14:22 +0100

    Add FileExtensions.cs and StringExtensions.cs

---
+++ Grimace.BulkInsert/Extensions/FileExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Grimace.BulkInsert.Extensions
+{
+  public static class FileExtensions
+  {
+    public static string CreateLink(this string path)
+    {
+      var link = "file://" + path.Replace('\\', '/');
+
+      return link;
+    }
+
+    public static string GetTempFilePath(this string extension)
+    {
+      string fullPath;
+      do
+      {
+        var path = Path.GetTempPath();
+        var fileName = Guid.NewGuid() + extension;
+
+        fullPath = Path.Combine(path, fileName);
+      }
+      while (File.Exists(fullPath));
+
+      return fullPath;
+    }
+  }
+}
---
+++ Grimace.BulkInsert/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grimace.BulkInsert.Extensions
+{
+  public static class StringExtensions
+  {
+    public static string Truncate(this string value, int maxLength)
+    {
+      if (value == null) throw new ArgumentNullException("value");
+      return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+    }
+  }
+}
