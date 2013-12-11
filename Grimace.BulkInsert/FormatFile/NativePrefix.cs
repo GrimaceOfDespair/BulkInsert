87749f3327411f34160b67701ef1a0da81b9bd26
Grimace of Despair <GrimaceOfDespair@hotmail.com>
2013-12-12 00:34:13 +0100

    Add GPL notice

--- Grimace.BulkInsert/FormatFile/NativePrefix.cs
+++ Grimace.BulkInsert/FormatFile/NativePrefix.cs
@@ -1,3 +1,22 @@
+﻿#region Copyright Notice
+// This file is part of Grimace.BulkInsert.
+// Bulk Insert into SQL without files
+// Copyright (C) 2013 Grimace of Despair
+// 
+// Grimace.BulkInsert is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Grimace.BulkInsert is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
 namespace Grimace.BulkInsert.FormatFile
 {
   public partial class NativePrefix
