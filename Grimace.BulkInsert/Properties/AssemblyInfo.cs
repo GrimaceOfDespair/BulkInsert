ba3d6799541b9c8d80e274c77bd66eb14eb9fd47
Grimace of Despair <GrimaceOfDespair@hotmail.com>
2013-12-12 02:24:18 +0100

    Fix compilation

--- Grimace.BulkInsert/Properties/AssemblyInfo.cs
+++ Grimace.BulkInsert/Properties/AssemblyInfo.cs
@@ -24,10 +24,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Grimace.BulkInsert")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BulkInsert")]
+[assembly: AssemblyDescription("Efficiently bulk insert data into SQLServer directly from .NET, without the hassle of import and format files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Grimace of Despair")]
 [assembly: AssemblyProduct("Grimace.BulkInsert")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
